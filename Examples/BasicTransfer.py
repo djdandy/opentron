e3656d03492a0ba7e9675f8f78494f36c7a888f1
Dandy <107224596+djdandy@users.noreply.github.com>
2022-08-04 11:50:43 -0400

    Update BasicTransfer.py

--- Examples/BasicTransfer.py
+++ Examples/BasicTransfer.py
@@ -5,6 +5,6 @@ metadata = {'apiLevel': '2.12'}
 def run(protocol: protocol_api.ProtocolContext):
     plate = protocol.load_labware('corning_96_wellplate_360ul_flat', 1)
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
-    p300 = protocol.load_instrument('p300_single', 'right', tip_racks=[tiprack_1])
+    p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
 
     p300.transfer(100, plate['A1'], plate['B1'])
