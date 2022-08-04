1f9784c9a8f3c2aa2bb122723a8c6a44c9b07957
Dandy <107224596+djdandy@users.noreply.github.com>
2022-08-04 11:50:33 -0400

    Update AirGaps.py

--- Examples/AirGaps.py
+++ Examples/AirGaps.py
@@ -6,7 +6,7 @@ def run(protocol: protocol_api.ProtocolContext):
     plate = protocol.load_labware('corning_96_wellplate_360ul_flat', 1)
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
     reservoir = protocol.load_labware('usascientific_12_reservoir_22ml', 4)
-    p300 = protocol.load_instrument('p300_single', 'right', tip_racks=[tiprack_1])
+    p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
 
     p300.pick_up_tip()
 
