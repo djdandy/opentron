5377d217d90957f9684ca917fea8f4bb081b33c4
Dandy <107224596+djdandy@users.noreply.github.com>
2022-09-13 15:10:40 -0400

    Create AB_SCIEX_P639088_100aliquots.py

---
+++ CurrentProjects/AB_SCIEX_P639088_100aliquots.py
@@ -0,0 +1,87 @@
+from opentrons import protocol_api
+
+metadata = {'apiLevel': '2.12',
+           'protocolName': 'AB SCIEX Aliquoting - Part 2',
+           'description': '''Aliquoting 100 vials of 50µL peptide pool from a single source''',
+           'author': 'Dylan D'}
+
+def run(protocol: protocol_api.ProtocolContext):
+    tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 4)
+    tube_rack_1 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 1)
+    plate_1 = protocol.load_labware('opentrons_24_tuberack_nest_1.5ml_screwcap', 2)
+    plate_2 = protocol.load_labware('opentrons_24_tuberack_nest_1.5ml_screwcap', 3)
+    plate_3 = protocol.load_labware('opentrons_24_tuberack_nest_1.5ml_screwcap', 5)
+    plate_4 = protocol.load_labware('opentrons_24_tuberack_nest_1.5ml_screwcap', 6)
+    plate_5 = protocol.load_labware('opentrons_24_tuberack_nest_1.5ml_screwcap', 7)
+    p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
+    
+    p300.well_bottom_clearance.dispense = 15
+
+    source=tube_rack_1.wells()[6]
+    source_vol=18150 ##CHANGE ME EVERY TIME TO MATCH STARTING VOLUME
+    remain_vol=source_vol
+    amounts=[50]
+    
+    p300.pick_up_tip()
+
+    for i in range(24):
+      destination=plate_1.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+    
+    remain_vol=remain_vol
+    amounts=[50]
+    
+    for i in range(24):
+      destination=plate_2.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+    
+    remain_vol=remain_vol
+    amounts=[50]
+    
+    for i in range(24):
+      destination=plate_3.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+    
+    remain_vol=remain_vol
+    amounts=[50]
+    
+    for i in range(24):
+      destination=plate_4.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+    
+    remain_vol=remain_vol
+    amounts=[50]
+    
+    for i in range(4):
+      destination=plate_5.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
