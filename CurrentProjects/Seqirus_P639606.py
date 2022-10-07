25a4f19a9a58a777b1eb7baf35aad7a2754e8b14
Dandy <107224596+djdandy@users.noreply.github.com>
2022-10-07 11:52:09 -0400

    Update Seqirus_P639606.py
    changed source vol

--- CurrentProjects/Seqirus_P639606.py
+++ CurrentProjects/Seqirus_P639606.py
@@ -16,7 +16,7 @@ def run(protocol: protocol_api.ProtocolContext):
     p300.well_bottom_clearance.dispense = 15
 
     source=tube_rack_1.wells()[0]
-    source_vol=18150
+    source_vol=2500
     remain_vol=source_vol
     amounts=[40]
     
