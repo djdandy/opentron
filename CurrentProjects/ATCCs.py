dbe1402cd0a5306c9916e8751b32e2f536fb6263
Dandy <107224596+djdandy@users.noreply.github.com>
2022-10-13 15:56:08 -0400

    Create ATCCs.py

---
+++ CurrentProjects/ATCCs.py
@@ -0,0 +1,45 @@
+from opentrons import protocol_api
+
+metadata = {'apiLevel': '2.12',
+           'protocolName': 'ATCC Aliquoting',
+           'description': '''Aliquoting 100 vials of solution from a single source''',
+           'author': 'Dylan D'}
+
+def run(protocol: protocol_api.ProtocolContext):
+    tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 4)
+    tube_rack_1 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 1)
+    plate_1 = protocol.load_labware('agilent_54_tuberack_2000ul', 2)
+    plate_2 = protocol.load_labware('agilent_54_tuberack_2000ul', 3)
+    p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
+    
+    p300.well_bottom_clearance.dispense = 15
+
+    source=tube_rack_1.wells()[0]
+    source_vol=2500
+    remain_vol=source_vol
+    amounts=[100]
+    
+    p300.pick_up_tip()
+
+    for i in range(54):
+      destination=plate_1.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+    
+    remain_vol=remain_vol
+    amounts=[100]
+    
+    for i in range(46):
+      destination=plate_2.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-3000
+      if temp_remain_vol>=3300:
+        p300.well_bottom_clearance.aspirate = (0.002*(temp_remain_vol)-6.2216)+17
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
