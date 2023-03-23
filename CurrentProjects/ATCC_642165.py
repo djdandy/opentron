d707a6a950f5410da4cdd12e698b7cfa31124c5e
Dandy <107224596+djdandy@users.noreply.github.com>
2023-03-23 15:44:38 -0400

    Create ATCC_642165.py

---
+++ CurrentProjects/ATCC_642165.py
@@ -0,0 +1,42 @@
+from opentrons import protocol_api
+
+metadata = {'apiLevel': '2.12',
+           'protocolName': 'ATCC Aliquoting',
+           'description': '''Aliquoting 60 vials of solution from a single source''',
+           'author': 'Dylan D'}
+
+def run(protocol: protocol_api.ProtocolContext):
+    tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 4)
+    tube_rack_1 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 1)
+    plate_1 = protocol.load_labware('agilent_54_tuberack_2000ul', 2)
+    plate_2 = protocol.load_labware('agilent_54_tuberack_2000ul', 3)
+    p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
+    
+    p300.well_bottom_clearance.dispense = 15
+
+    source=tube_rack_1.wells()[0]
+    source_vol=10500 #Only change me according to how much µL of peptide you have!
+    remain_vol=source_vol
+    amounts=[100]
+    
+    p300.pick_up_tip()
+
+    for i in range(54):
+      destination=plate_1.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-400
+      if temp_remain_vol>=1500:
+        p300.well_bottom_clearance.aspirate = (0.006*(temp_remain_vol)-8.7638)+23
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+    
+    for i in range(6):
+      destination=plate_2.wells()[i]
+      remain_vol=remain_vol-amounts[0]
+      temp_remain_vol=(remain_vol)-400
+      if temp_remain_vol>=1500:
+        p300.well_bottom_clearance.aspirate = (0.006*(temp_remain_vol)-8.7638)+23
+      else:
+        p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
