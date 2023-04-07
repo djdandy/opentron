16ff1a79a6db41d2b2774998465935e1f264eace
Dandy <107224596+djdandy@users.noreply.github.com>
2023-04-07 18:01:29 -0400

    Update Renamer.py
    Should be all good to use - we can work out quirks later 😄

--- Renamer.py
+++ Renamer.py
@@ -1,21 +1,25 @@
+#Use by placing this file/code in the folder with all the files that need to be renamed - it's doing all relative work.
+
 import os
 
-File="AAA_1776.ars"
-Data=open(File,"r")
+for File in os.listdir():
+    #print(File)
+    if File.endswith(".ars"):
+        Data=open(File,'r')
+        Data.readline()
+        Data.readline()
+        Data.readline()
+        SampleLine=Data.readline()
+        #print(SampleLine)
+        One=SampleLine.find('"1"')
+        SampleBegin=SampleLine.find('"',One+3)
+        SampleEnd=SampleLine.find('"',SampleBegin+1)
+        SampleName=SampleLine[SampleBegin+1:SampleEnd]+".ars"
+        Data.close()
+        #print(FileName)
+        #print(SampleName)
+        FixedSampleName=SampleName.replace("/","&")
+        #print(FixedSampleName)
+        os.rename(File,FixedSampleName)
 
-Data.readline()
-Data.readline()
-Data.readline()
-Fourth=Data.readline()
-one=Fourth.find('"1"')
-SampleBegin=Fourth.find('"',one+3)
-SampleEnd=Fourth.find('"',SampleBegin+1)
-SampleName=Fourth[SampleBegin+1:SampleEnd]+".ars"
-Data.close()
 
-print(one)
-print(SampleBegin)
-print(SampleEnd)
-print(SampleName)
-
-os.rename(File,SampleName)
