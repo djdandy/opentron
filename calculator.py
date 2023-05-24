76136b3ec61f3f2053710c8575148bb3affe83b1
Dandy <107224596+djdandy@users.noreply.github.com>
2023-05-24 18:07:38 -0400

    Update calculator.py
    changed round() to math.floor() - did this so the tip always goes a little deeper than recommended rather than rounding up and going higher up than intended.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 print('Is the source a... \n a) 15mL conical \n b) 50mL conical')
 choice=input()
 if choice=='a':
@@ -9,7 +11,7 @@ if choice=='a':
     ali_num=float(input())
     remain=(source-ali_vol*ali_num)-400
     if remain>=1500:
-        depth=round((0.006*(remain)-8.7638)+23,0)
+        depth=math.floor((0.006*(remain)-8.7638)+23)
         print('Set aspiration tip position to '+str(depth)+' mm')
     else:
         print('Set aspiration tip position to 1.5 mm')
@@ -22,7 +24,7 @@ elif choice=='b':
     ali_num=float(input())
     remain=(source-ali_vol*ali_num)-400
     if remain>=3300:
-        depth=round((0.002*(remain)-6.2216)+17,0)
+        depth=math.floor((0.002*(remain)-6.2216)+17)
         print('Set aspiration tip position to '+str(depth)+' mm')
     else:
         print('Set aspiration tip position to 1.5 mm')
