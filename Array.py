70a01d0434cf5fc848d2e75983f5bb106fe6a7e6
Dandy <107224596+djdandy@users.noreply.github.com>
2023-05-29 15:24:43 -0400

    Update Array.py
    Fixed formatting/alignment issues and changed labware definitions to accurately represent what would be used.

--- Array.py
+++ Array.py
@@ -7,14 +7,14 @@ metadata = {'apiLevel': '2.12',
 
 def run(protocol: protocol_api.ProtocolContext):
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
-    tube_rack_1 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 1)
-    tube_rack_2 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 4)
-    tube_rack_3 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 7)
-    tube_rack_4 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 10)
-    tube_rack_5 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 11)
-    tube_rack_6 = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 8)
-    plate_1 = protocol.load_labware('agilent_54_tuberack_2000ul', 6)
-    plate_2 = protocol.load_labware('agilent_54_tuberack_2000ul', 3)
+    tube_rack_1 = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 1)
+    tube_rack_2 = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 4)
+    tube_rack_3 = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 7)
+    tube_rack_4 = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 10)
+    tube_rack_5 = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 11)
+    tube_rack_6 = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 8)
+    plate_1 = protocol.load_labware('nest_96_wellplate_2ml_deep', 6)
+    plate_2 = protocol.load_labware('nest_96_wellplate_2ml_deep', 3)
     p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
     
     p300.well_bottom_clearance.dispense = 15
@@ -33,7 +33,7 @@ def run(protocol: protocol_api.ProtocolContext):
       
     for i in range(15):
       p300.pick_up_tip()
-      source=tube_rack_2.wells()[i+15]
+      source=tube_rack_2.wells()[i]
       destination=plate_1.wells()[i+15]
       p300.well_bottom_clearance.aspirate = 1.5
       p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
@@ -43,7 +43,7 @@ def run(protocol: protocol_api.ProtocolContext):
       
     for i in range(15):
       p300.pick_up_tip()
-      source=tube_rack_3.wells()[i+15+15]
+      source=tube_rack_3.wells()[i]
       destination=plate_1.wells()[i+15+15]
       p300.well_bottom_clearance.aspirate = 1.5
       p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
@@ -51,32 +51,32 @@ def run(protocol: protocol_api.ProtocolContext):
       p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
       p300.drop_tip()
       
-      for i in range(15):
-        p300.pick_up_tip()
-        source=tube_rack_4.wells()[i+15+15+15]
-        destination=plate_1.wells()[i+15+15+15]
-        p300.well_bottom_clearance.aspirate = 1.5
-        p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
-        destination=plate_2.wells()[i+15+15+15]
-        p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
-        p300.drop_tip()
+    for i in range(15):
+      p300.pick_up_tip()
+      source=tube_rack_4.wells()[i]
+      destination=plate_1.wells()[i+15+15+15]
+      p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+      destination=plate_2.wells()[i+15+15+15]
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+      p300.drop_tip()
       
-      for i in range(15):
-        p300.pick_up_tip()
-        source=tube_rack_5.wells()[i+15+15+15+15]
-        destination=plate_1.wells()[i+15+15+15+15]
-        p300.well_bottom_clearance.aspirate = 1.5
-        p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
-        destination=plate_2.wells()[i+15+15+15+15]
-        p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
-        p300.drop_tip()
+    for i in range(15):
+      p300.pick_up_tip()
+      source=tube_rack_5.wells()[i]
+      destination=plate_1.wells()[i+15+15+15+15]
+      p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+      destination=plate_2.wells()[i+15+15+15+15]
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+      p300.drop_tip()
       
-      for i in range(6):
-        p300.pick_up_tip()
-        source=tube_rack_6.wells()[i+15+15+15+15+15]
-        destination=plate_1.wells()[i+15+15+15+15+15]
-        p300.well_bottom_clearance.aspirate = 1.5
-        p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
-        destination=plate_2.wells()[i+15+15+15+15+15]
-        p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
-        p300.drop_tip()
+    for i in range(6):
+      p300.pick_up_tip()
+      source=tube_rack_6.wells()[i]
+      destination=plate_1.wells()[i+15+15+15+15+15]
+      p300.well_bottom_clearance.aspirate = 1.5
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+      destination=plate_2.wells()[i+15+15+15+15+15]
+      p300.distribute(amounts[0], source, destination, new_tip = 'never', blow_out = True, blowout_location = 'source well')
+      p300.drop_tip()
